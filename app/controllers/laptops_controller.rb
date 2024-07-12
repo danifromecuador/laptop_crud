ed3dc9ed282f02fc2322851cbd80eb52766a77c9
dani <danifromecuador@gmail.com>
2024-07-11 23:56:05 -0500

    Implement create feature for Laptop model branch: feat/project-creation

--- app/controllers/laptops_controller.rb
+++ app/controllers/laptops_controller.rb
@@ -6,4 +6,23 @@ class LaptopsController < ApplicationController
   def show
     @laptop = Laptop.find(params[:id])
   end
+
+  def new
+    @laptop = Laptop.new
+  end
+
+  def create
+    @laptop = Laptop.new(laptop_params)
+
+    if @laptop.save
+      redirect_to "/laptops"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def laptop_params
+    params.require(:laptop).permit(:lbrand, :lmodel)
+  end
 end
