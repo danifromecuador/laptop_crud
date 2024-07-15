63be2c8545d2ceb39902c03c67245f122932a4b8
dani <danifromecuador@gmail.com>
2024-07-15 13:17:06 -0500

    Add gems to render script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,12 @@
 set -o errexit
 
 bundle install
+
+gem install net-pop -v '~> 0.1.2'
+gem install net-protocol -v '~> 0.1.3'
+
+
+
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
